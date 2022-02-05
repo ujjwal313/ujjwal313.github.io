57948192359ed33fd6991858c3771ced88059bbf
Ujjwal Kumar <ujjwalkr80@gmail.com>
2022-02-05 23:42:55 +0530

    added cursor circle

--- index.js
+++ index.js
@@ -31,3 +31,30 @@ for (let i = 0; i < headerSmallMenuLinks.length; i++) {
     headerHamMenuCloseBtn.classList.add("hidden");
   });
 }
+
+
+if (window.matchMedia("(min-width: 768px)").matches) {
+  let mousePosX = 0,
+    mousePosY = 0,
+    mouseCircle = document.getElementById("cursor");
+
+  document.onmousemove = (e) => {
+    mousePosX = e.pageX;
+    mousePosY = e.pageY;
+  };
+
+  let delay = 1,
+    revisedMousePosX = 0,
+    revisedMousePosY = 0;
+
+  function delayMouseFollow() {
+    requestAnimationFrame(delayMouseFollow);
+
+    revisedMousePosX += (mousePosX - revisedMousePosX) / delay;
+    revisedMousePosY += (mousePosY - revisedMousePosY) / delay;
+
+    mouseCircle.style.top = revisedMousePosY + "px";
+    mouseCircle.style.left = revisedMousePosX + "px";
+  }
+  delayMouseFollow();
+}
